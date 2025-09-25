4c0db70822340e5767273f3e28fd1caa197ec516
Mauricio Oliveira <mau_oliver@hotmail.com>
2025-09-25 14:51:37 -0300

    fix: adicionar último URL do frontend no CORS

--- src/app.js
+++ src/app.js
@@ -58,6 +58,7 @@ app.use(cors({
     'https://frontend-n8oxehapg-mauricio-mp-oliveiras-projects.vercel.app',
     'https://frontend-8351bqycr-mauricio-mp-oliveiras-projects.vercel.app',
     'https://frontend-lznaudnp2-mauricio-mp-oliveiras-projects.vercel.app',
+    'https://frontend-erztz6jv7-mauricio-mp-oliveiras-projects.vercel.app',
     process.env.CORS_ORIGIN || 'https://your-frontend.vercel.app',
     null // Permitir arquivos HTML locais (origin 'null')
   ],
